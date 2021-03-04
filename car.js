b6435166c341936af7870ba64405a3618dc6a595
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-03-05 01:10:41 +0600

    ver 2

--- car.js
+++ car.js
@@ -1,65 +1,37 @@
-class Car{
-
-
-  constructor()
-  {
-     this.x=width/2;
-     this.y=height-60;
-     this.life=3;
-     
-
+class Car {
+  constructor() {
+    this.x = width / 2 - 75;
+    this.y = height - 60;
+    this.life = 3;
   }
 
-
-  lifeChange()
-  {
+  lifeChange() {
     this.life--;
 
     return this.life;
   }
 
-  show()
-  {
-  ///  console.log("sho");
+  show() {
+    ///  console.log("sho");
 
-  if(this.life===3)
-  {
-    fill(0,250,0)
-  }
-  else if(this.life===2)
-  {
-    fill(255,255,0);
-
-  }
-
-  else if(this.life===1)
-  {
-    fill(255,0,0);
-
-  }
-  
-   
-    rect(this.x,this.y,150,50);
-  }
-
-  move(l)
-  {
-
-    if(this.x+100<width-19 && l>0)
-    {
-      this.x+=l;
+    if (this.life === 3) {
+      fill(0, 250, 0);
+    } else if (this.life === 2) {
+      fill(255, 255, 0);
+    } else if (this.life === 1) {
+      fill(255, 0, 0);
     }
 
-    if(this.x+l>0 && l<0)
-    {
-      this.x+=l;
-    }
-   
-
+    rect(this.x, this.y, 150, 50);
   }
 
+  move(l) {
+    if (this.x + 100 < width - 19 && l > 0) {
+      this.x += l;
+    }
 
-
-
-
+    if (this.x + l > 0 && l < 0) {
+      this.x += l;
+    }
+  }
 }
